586c07d3f94e865169a77e346a335c119f080bb3
Jon Deng <jonathan.deng@creditkarma.com>
2019-05-02 18:38:39 -0700

    add minerals class

---
+++ src/FoodItemMinerals.java
@@ -0,0 +1,15 @@
+public class FoodItemMinerals {
+    public Double calcium;
+    public Double fiber;
+    public Double sugar;
+    public Double saturatedFat;
+    public Double linoleicAcid;
+    public Double linolenicAcid;
+    public Double Iron;
+    public Double vitaminA;
+    public Double vitaminB;
+    public Double vitaminD;
+    public Double VitaminC;
+    public Double vitaminB6;
+    public Double vitaminB12;
+}
